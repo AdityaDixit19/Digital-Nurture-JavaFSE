76ab550622dd2e49462276c238605ebc79124e22
Aditya Dixit <adityadixit19@gmail.com>
2026-07-23 11:30:36 +0530

    done with jwt hand-on

---
+++ Deepskilling/Week-03-Assignments/JWT/authentication_JWT/src/main/java/com/example/authentication_JWT/authentication_JWTApplication.java
@@ -0,0 +1,12 @@
+package com.example.authentication_JWT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class authentication_JWTApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(authentication_JWTApplication.class, args);
+	}
+}
