edc7c211192ad40050b888e9dca5fe6c72995878
Aditya Dixit <optional1618@gmail.com>
2026-07-28 03:00:23 +0530

    changes made in folder name and structure

--- Deepskilling/Week-06-React & Git/11. ReactJS-HOL/eventexamplesapp/src/App.js
+++ Deepskilling/Week-06-React & Git/11. ReactJS-HOL/eventexamplesapp/src/App.js
@@ -1,21 +1,8 @@
-import React from 'react';
-import Counter from './Components/Counter';
-import WelcomeButton from './Components/WelcomeButton';
-import SyntheticEventButton from './Components/SyntheticEventButton';
-import CurrencyConvertor from './Components/CurrencyConvertor';
-
+import LoginControl from "./LoginControl";
 function App() {
   return (
-    <div className="App">
-      <h1>eventexamplesapp</h1>
-      <p style={{ textAlign: 'center', color: '#555' }}>
-        React Event Handling Lab
-      </p>
-
-      <Counter />
-      <WelcomeButton />
-      <SyntheticEventButton />
-      <CurrencyConvertor />
+    <div>
+      <LoginControl />
     </div>
   );
 }
